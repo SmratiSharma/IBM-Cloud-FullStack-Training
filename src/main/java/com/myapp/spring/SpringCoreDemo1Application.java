5194eaa1fd4fb3e4ac1a52aee4f46906249531c9
Smrati Sharma <smrati.sharma@ibm.com>
2026-08-20 10:19:11 +0530

    Application Context and App Config

---
+++ src/main/java/com/myapp/spring/SpringCoreDemo1Application.java
@@ -0,0 +1,22 @@
+package com.myapp.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.myapp.spring.config.AppConfig;
+import com.myapp.spring.service.MovieSearchService;
+
+//@SpringBootApplication
+public class SpringCoreDemo1Application {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(SpringCoreDemo1Application.class, args);
+		
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		MovieSearchService searchService = context.getBean(MovieSearchService.class);
+		searchService.search("The Matrix").forEach(System.out::println);
+	
+	}
+}
